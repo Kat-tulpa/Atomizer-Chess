61324e8ddc993ae52eecdeb8ff4f11532d8ef05e
Kat-tulpa <56337080+Kat-tulpa@users.noreply.github.com>
2023-04-02 11:46:19 -0700

    Now multithreaded
    Number of threads are automatically detected based on CPU

--- SequenceManager.h
+++ SequenceManager.h
@@ -2,6 +2,9 @@
 #include <string_view>
 #include <unordered_map>
 #include <tuple>
+#include <mutex>
+#include <thread>
+#include <vector>
 
 namespace SequenceManager {
     constexpr char notation[] =
@@ -10,7 +13,9 @@ namespace SequenceManager {
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     constexpr size_t differentTypes = sizeof(notation) - 1;
 
+    // Using a thread-safe container and a mutex to protect access to it
     static std::unordered_map<std::string, std::tuple<unsigned int, int>> sequence_occurances_score;
+    static std::mutex map_mutex;
 
     template <typename CharType = char, const char* Notation = notation>
     std::basic_string<CharType> makeRandom(size_t length) {
@@ -25,6 +30,7 @@ namespace SequenceManager {
     }
 
     static void add(const std::string& sequence) {
+        std::lock_guard<std::mutex> guard(map_mutex);
         auto it = sequence_occurances_score.find(sequence);
         if (it != sequence_occurances_score.end()) {
             std::get<0>(it->second)++;
--- main.cpp
+++ main.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
 #include "SequenceManager.h"
 
-static constexpr size_t RAND_PIECESEQUENCE_LENGTH = 9;
+static constexpr size_t RAND_PIECESEQUENCE_LENGTH = 2;
+static unsigned int THREAD_COUNT = std::thread::hardware_concurrency();;
 
 void addRandomSequences(const unsigned int count, const size_t length) {
-	for (unsigned int i = 0; i < count; i++)
-		SequenceManager::add(SequenceManager::makeRandom(length));
+    const unsigned int countPerThread = count / THREAD_COUNT;
+    std::vector<std::thread> threads;
+    threads.reserve(THREAD_COUNT);
+    for (unsigned int i = 0; i < THREAD_COUNT - 1; i++) {
+        threads.emplace_back([&]() {
+            for (unsigned int j = 0; j < countPerThread; j++) {
+                SequenceManager::add(SequenceManager::makeRandom(length));
+            }
+            });
+    }
+    for (unsigned int j = 0; j < countPerThread + count % THREAD_COUNT; j++) {
+        SequenceManager::add(SequenceManager::makeRandom(length));
+    }
+    for (auto& thread : threads) {
+        thread.join();
+    }
 }
 
 void printSequences() {
-	for (const auto& entry : SequenceManager::sequence_occurances_score) {
-		std::cout << entry.first << " : " << std::get<0>(entry.second) << std::endl;
-	}
+    std::lock_guard<std::mutex> guard(SequenceManager::map_mutex);
+    for (const auto& entry : SequenceManager::sequence_occurances_score) {
+        std::cout << entry.first << " : " << std::get<0>(entry.second) << std::endl;
+    }
 }
 
 int main() {
-	addRandomSequences(1000000, RAND_PIECESEQUENCE_LENGTH);
-	printSequences();
-	return 0;
+    addRandomSequences(1000000, RAND_PIECESEQUENCE_LENGTH);
+    printSequences();
+    return 0;
 }
