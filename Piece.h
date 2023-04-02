b2bd5ec725e253f9f64417ac442e3ce68e8c940a
Kat-tulpa <56337080+Kat-tulpa@users.noreply.github.com>
2023-04-02 14:55:38 -0700

    Various fixes

--- Piece.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Piece {
-	static const char notation[] =
-		"0123456789"
-		"abcdefghijklmnopqrstuvwxyz"
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	size_t differentTypes = sizeof(notation) - 1;
-}
--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include "ChessSequence.h"
 
-static constexpr size_t RAND_PIECESEQUENCE_LENGTH = 2;
+static constexpr size_t RAND_PIECESEQUENCE_LENGTH = 64;
 
 void printSequences() {
-    for (const auto& entry : SequenceManager::sequence_occurances_score) {
+    for (const auto& entry : SequenceManager::sequence_map) {
         std::cout << entry.first << " : " << std::get<0>(entry.second) << std::endl;
     }
 }
 
 int main() {
-    ChessSequenceManager::addRandom(1000, RAND_PIECESEQUENCE_LENGTH);
+    ChessSequenceManager::addRandom(1000000, RAND_PIECESEQUENCE_LENGTH);
     printSequences();
     return 0;
 }
