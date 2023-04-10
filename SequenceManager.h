4262a8d4edd45d111cf91f9262eb3aa755ea71b2
Kat-tulpa <56337080+Kat-tulpa@users.noreply.github.com>
2023-04-10 03:00:18 -0700

    Major refactoring, still works the same in essence however

--- SequenceManager.h
+++ SequenceManager.h
@@ -6,21 +6,47 @@
 class SequenceManager {
 private:
 
-    // First few bits of std::string = readable length of string in bits, anything after is raw bit data
-    static std::vector<std::unordered_map<std::string, std::string>> sequence_maps;
+    using data_occurrence = std::pair<std::string, uint8_t>;
+
+    // First few bits of std::string = readable length of std::string in bits, anything after is raw bit data
+    static std::vector<std::unordered_map<std::string, data_occurrence>> sequence_maps;
 
 public:
 
     static void print() {
+        // Define a lambda function to print the total number of entries
+        auto printTotalEntries = [](int numEntries, int sumValues) {
+            std::cout << "Total number of entries: " << numEntries << std::endl;
+            std::cout << "Sum of all values: " << sumValues << std::endl;
+        };
+
+        // Initialize counters for the total number of entries and the sum of all key values
+        int totalEntries = 0;
+        int sumValues = 0;
+
         // Iterate over each map in the vector
+        for (const auto& map : sequence_maps) {
+            // Add the number of entries in the current map to the total counter
+            totalEntries += map.size();
+
+            // Iterate over each key-value pair in the map and add its value to the sum counter
+            for (const auto& pair : map) {
+                sumValues += pair.second.second;
+            }
+        }
+
+        // Call the lambda function to print the total number of entries and the sum of all key values
+        printTotalEntries(totalEntries, sumValues);
+
+        // Iterate over each map in the vector again and print out its entries
         for (const auto& map : sequence_maps) {
             // Print the opening bracket for the map
             std::cout << "{\n";
 
             // Iterate over each key-value pair in the map
             for (const auto& pair : map) {
-                // Print the key and value as a string, with quotes around them
-                std::cout << "    \"" << pair.first << "\": \"" << pair.second << "\",\n";
+                // Print the key and value as a std::string, with quotes around them
+                std::cout << "\"" << pair.second.first << "\" : " << static_cast<unsigned int>(pair.second.second) << "," << std::endl;
             }
 
             // Print the closing bracket for the map
@@ -30,29 +56,22 @@ public:
 
     // Adds a new hashmap for a new type of data, returning the new index of the hashmap that's being added
     static const uint8_t makeAddDataType() {
-        std::unordered_map<std::string, std::string> typeToAdd;
+        std::unordered_map<std::string, std::pair<std::string, uint8_t>> typeToAdd;
         sequence_maps.push_back(typeToAdd);
         return sequence_maps.size() - 1;
     }
 
-    // Function to add a sequence to the map
-    static void addDataEntry(uint8_t dataType, const std::string& data) {
-        auto& map = sequence_maps[dataType];
-        auto it = map.find(data);
-        if (it == map.end())
-            map.emplace(data, data);
-        //print();
-    }
-
-    // Function to add a sequence to the map
-    static void addDataEntries(uint8_t dataType, const std::vector<std::string>& datas) {
-        auto& map = sequence_maps[dataType];
-        for (const auto& data : datas) {
-            auto it = map.find(data);
-            if (it == map.end())
-                map.emplace(data, data);
+    static void addDataEntry(const uint8_t registrationID, std::string entry) {
+        auto& map = sequence_maps[registrationID];
+        auto it = map.find(entry);
+        if (it == map.end()) {
+            data_occurrence pair = std::make_pair(entry, 1);
+            map.emplace(entry, pair);
+        }
+        else {
+            it->second.second++;
         }
     }
 };
 
-std::vector<std::unordered_map<std::string, std::string>> SequenceManager::sequence_maps; // Implementation needs to be defined outside of class declarr
+std::vector<std::unordered_map<std::string, std::pair<std::string, uint8_t>>> SequenceManager::sequence_maps; // Implementation needs to be defined outside of class declaration
