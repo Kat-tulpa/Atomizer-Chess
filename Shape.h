910136458b04f26febdd966ac58f8f9d00c7d502
Kat-tulpa <56337080+Kat-tulpa@users.noreply.github.com>
2023-04-21 13:32:15 -0700

    added class hash and compare functions, copy constructor. Removed some getters and setters which were unneeded.

--- Shape.h
+++ Shape.h
@@ -2,34 +2,25 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <functional>
 
 #include "Utility.h"
 
 class Shape {
+
 public:
-
     struct Dimensions {
-    public:
         Dimensions(uint8_t width, uint8_t height) : m_width(width), m_height(height) {}
+        auto getCategorizationPrefix() const { return std::string(2, static_cast<char>(m_width) + static_cast<char>(m_height)); }
 
-        auto getWidth() const { return m_width; }
-        auto getHeight() const { return m_height; }
-        auto getCategorizationPrefix() const { return std::string(2, static_cast<char>(getWidth()) + static_cast<char>(getHeight())); }
-
-    private:
         uint8_t m_width;
         uint8_t m_height;
     };
 
     struct Offset {
-    public:
         Offset(uint8_t x, uint8_t y) : m_x(x), m_y(y) {}
+        auto getCategorizationPrefix() const { return std::string(2, static_cast<char>(m_x) + static_cast<char>(m_y)); }
 
-        auto getX() const { return m_x; }
-        auto getY() const { return m_y; }
-        auto getCategorizationPrefix() const { return std::string(2, static_cast<char>(getX()) + static_cast<char>(getY())); }
-
-    private:
         uint8_t m_x;
         uint8_t m_y;
     };
@@ -48,41 +39,32 @@ public:
         TYPE_COUNT
     };
 
-private:
-    const Type m_type;
-    const Dimensions m_dimensions;
-    const Offset m_offset;
-    const std::vector<char> m_data;
-
-public:
-
     Shape(const Type type, const Dimensions dimensions, const Offset offset, std::vector<char> data)
         : m_type(type), m_dimensions(dimensions), m_offset(offset), m_data(data)
-    {};
+    {}
 
-    bool operator==(const Shape& lhs, const Shape& rhs) {
-        return lhs.getDimensions().getWidth() == rhs.getDimensions().getWidth()
-            && lhs.getDimensions().getHeight() == rhs.getDimensions().getHeight()
-            && lhs.getOffset().getX() == rhs.getOffset().getX()
-            && lhs.getOffset().getY() == rhs.getOffset().getY()
-            && lhs.getData() == rhs.getData()
-            && lhs.getType() == rhs.getType();
+    Shape& operator=(const Shape& other) {
+        if (this == &other) {
+            return *this;
+        }
+        m_type = other.m_type;
+        m_dimensions.m_width = other.m_dimensions.m_width;
+        m_dimensions.m_height = other.m_dimensions.m_height;
+        m_offset.m_x = other.m_offset.m_x;
+        m_offset.m_y = other.m_offset.m_y;
+        m_data = other.m_data;
+        return *this;
     }
 
-    bool operator!=(const Shape& lhs, const Shape& rhs) {
-        return !(lhs == rhs);
-    }
-
-    auto getDimensions() const { return m_dimensions; }
-    auto getWidth() const { return m_dimensions.getWidth(); }
-    auto getHeight() const { return m_dimensions.getHeight(); }
-    auto getOffset() const { return m_offset; }
-    auto getOffsetX() const { return m_offset.getX(); }
-    auto getOffsetY() const { return m_offset.getY(); }
+    auto getType() const { return m_type; }
+    auto getWidth() const { return m_dimensions.m_width; }
+    auto getHeight() const { return m_dimensions.m_height; }
+    auto getOffsetX() const { return m_offset.m_x; }
+    auto getOffsetY() const { return m_offset.m_y; }
     auto getData() const { return m_data; }
 
     auto getCategorizationPrefix() const { 
-        return getDimensions().getCategorizationPrefix() + getOffset().getCategorizationPrefix(); 
+        return m_dimensions.getCategorizationPrefix() + m_offset.getCategorizationPrefix(); 
     }
 
     bool containsSubshape(const Shape& subshape) {
@@ -108,4 +90,39 @@ public:
 
         return true;
     }
+
+    Type m_type;
+    Dimensions m_dimensions;
+    Offset m_offset;
+    std::vector<char> m_data;
 };
+
+namespace std {
+    template <>
+    struct hash<Shape> {
+        size_t operator()(const Shape& s) const {
+            size_t hash_value = static_cast<size_t>(s.getType());
+            hash_value ^= static_cast<size_t>(s.getWidth()) << 8;
+            hash_value ^= static_cast<size_t>(s.getHeight()) << 16;
+            hash_value ^= static_cast<size_t>(s.getOffsetX()) << 24;
+            hash_value ^= static_cast<size_t>(s.getOffsetY()) << 32;
+            const auto& data = s.getData();
+            for (const char& c : data) {
+                hash_value ^= static_cast<size_t>(c) << 40;
+            }
+            return hash_value;
+        }
+    };
+
+    template <>
+    struct equal_to<Shape> {
+        bool operator()(const Shape& s1, const Shape& s2) const {
+            return s1.getType() == s2.getType()
+                && s1.m_dimensions.m_width == s2.m_dimensions.m_width
+                && s1.m_dimensions.m_height == s2.m_dimensions.m_height
+                && s1.m_offset.m_x == s2.m_offset.m_x
+                && s1.m_offset.m_y == s2.m_offset.m_y
+                && s1.getData() == s2.getData();
+        }
+    };
+}
